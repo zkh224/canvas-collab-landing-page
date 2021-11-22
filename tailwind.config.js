5ef6549545a018e2b521a6a5e80a27834bbca170
zkh224 <khanzahra06@hotmail.com>
2021-11-21 23:53:02 -0500

    Navbar and hero init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
   theme: {
     extend: {
       colors: {
-        "bookmark-purple": "#5267DF",
-        "bookmark-red": "#FA5959",
-        "bookmark-blue": "#242A45",
-        "bookmark-grey": "#9194A2",
-        "bookmark-white": "#f7f7f7",
-      },
-    },
-    fontFamily: {
-      Poppins: ["Poppins, sans-serif"],
-    },
-    container: {
-      center: true,
-      padding: "1rem",
-      screens: {
-        lg: "1124px",
-        xl: "1124px",
-        "2xl": "1124px",
+        sky: colors.sky,
+        cyan: colors.cyan,
       },
     },
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
-};
+}
