3b04c60a26a4909740c9026c0d0772425d96531d
zkh224 <khanzahra06@hotmail.com>
2021-11-25 01:43:33 -0500

    Added background image

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,27 +6,20 @@ const Content = () => {
         <>
         <div className='container mx-auto flex flex-wrap p-10 justify-center'>
                 <p className="w-full my-2 text-3xl leading-tight text-red-400 text-center">Simple to use. A remote tool for anyone. </p>
-            <img className='w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink' src="whiteboard.png" alt='video of the whiteboard demostration'></img>
+            <img className='w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink' src="whiteboard.png" alt='snapshot of the whiteboard demostration'></img>
         </div>
         
         <Cards />
         
-        <div className='flex flex-wrap md:h-screen justify-center items-center p-10'>
-            <div class="w-full overflow-hidden md:w-1/4">
+        <div className='md:h-screen flex flex-row justify-center items-center p-10 gap-2' style={{ backgroundImage: "url(/linesbg.svg)", backgroundSize: 'contain' }}>
+            <div class="w-full md:w-2/5">
                 <img src='https://images.unsplash.com/photo-1637517626826-a2e2ac630622?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='first demostration of the whiteboard' />
             </div>
             
-            <div class="w-full overflow-hidden md:w-1/4">
+            <div class="w-full md:w-2/5">
                 <img src='https://images.unsplash.com/photo-1636955890525-84c5fa482c85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80' alt='second demostration of the whiteboard' />
             </div>
             
-            <div class="w-full overflow-hidden md:w-1/4">
-                <img src='https://images.unsplash.com/photo-1637441511291-d5994ea09a6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80' alt='third demostration of the whiteboard' />
-            </div>
-            
-            <div class="w-full overflow-hidden md:w-1/4">
-                <img src='https://images.unsplash.com/photo-1637481108969-836f76676413?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1462&q=80' alt='fourth demostration of the whiteboard' />
-            </div>
         </div>
         
         <div className='bg-yellow-50 py-10'>
