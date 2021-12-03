352032b95b1401415eea55ca2587616099e60dd6
zkh224 <khanzahra06@hotmail.com>
2021-12-03 00:37:16 -0500

    responsive navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = ({toggle}) => {
         <nav className='flex justify-between items-center h-12 px-4 bg-white text-black relative shadow' role='navigation'>
           <Link className='inline-flex gap-2' to= '/'> 
               <img className='h-10 w-10' src='logoCC.png' alt='logo of Canvas Collab' />
-            <div className='text-2xl font-bold leading-10 text-center uppercase'>Canvas Collab</div>
+            <div className='text-xl md:text-2xl font-bold leading-10 text-center uppercase'>Canvas Collab</div>
           </Link>
           <div className='px-4 cursor-pointer md:hidden' onClick={toggle}>  
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
